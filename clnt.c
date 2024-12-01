294e50fd6a4760968cb4d50adc470202e0700182
root <22000082@handong.ac.kr>
2024-12-01 16:31:50 +0000

    dir create

--- clnt.c
+++ clnt.c
@@ -4,69 +4,110 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <pthread.h>
+#include <dirent.h>
+#include <signal.h>
 
 #define FILE_LEN 32
 #define BUF_SIZE 1024
+
 void error_handling(char *message);
 
-int main(int argc, char *argv[])
-{
-	int sd;
-	FILE *fp;
-	
-	char file_name[FILE_LEN];
-	char buf[BUF_SIZE];
-	int read_cnt;
-	int read_size;
-	struct sockaddr_in serv_adr;
-	if (argc != 4) {
-		printf("Usage: %s <IP> <port> <file name> \n", argv[0]);
-		exit(1);
-	}
-	
-	fp = fopen(argv[3], "rb");
-	sd = socket(PF_INET, SOCK_STREAM, 0);   
+typedef struct {
+    int sock;
+    char file_path[FILE_LEN];
+} upload_arg;
 
-	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family = AF_INET;
-	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_adr.sin_port = htons(atoi(argv[2]));
+void *upload_file(void *arg) {
+    upload_arg *u_arg = (upload_arg *)arg;
+    int sock = u_arg->sock;
+    char *file_path = u_arg->file_path;
 
-	connect(sd, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
-	
-	// Send file name 
-	strcpy(file_name, argv[3]);
-	write(sd, file_name, FILE_LEN);
+    FILE *fp = fopen(file_path, "rb");
+    if (!fp) {
+        perror("File open error");
+        free(u_arg);
+        return NULL;
+    }
 
-	// Send file data 
-	read_size = 0;
-	while(1)
-	{
-		read_cnt = fread((void*)buf, 1, BUF_SIZE, fp);
-		read_size += read_cnt;
-		if (read_size % 1024 == 0)
-			printf("Send %d bytes \n", read_size);
+    // Send file name
+    write(sock, file_path, sizeof(file_path));
 
-		if (read_cnt < BUF_SIZE)
-		{
-			write(sd, buf, read_cnt);
+    char buf[BUF_SIZE];
+    int read_cnt;
+    long total_bytes = 0;
 
-			break;
-		}
-		write(sd, buf, BUF_SIZE);
-	}
+    while ((read_cnt = fread(buf, 1, BUF_SIZE, fp)) > 0) {
+        write(sock, buf, read_cnt);
+        total_bytes += read_cnt;
+    }
 
-	
-	shutdown(sd, SHUT_WR);
+    fclose(fp);
+    free(u_arg);
 
-	fclose(fp);
-	close(sd);
-	return 0;
+    printf("[%s] Upload completed! … %ld bytes\n", file_path, total_bytes);
+    return NULL;
 }
 
-void error_handling(char *message)
-{
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
+int main(int argc, char *argv[]) {
+    int sd;
+    struct sockaddr_in serv_adr;
+
+    if (argc != 4) {
+        printf("Usage: %s <IP> <port> <dir name>\n", argv[0]);
+        exit(1);
+    }
+
+    sd = socket(PF_INET, SOCK_STREAM, 0);   
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
+
+    connect(sd, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+
+    DIR *dir = opendir(argv[3]);
+    if (dir == NULL) {
+        error_handling("Directory open error");
+    }
+
+    // Send directory name
+    write(sd, argv[3], FILE_LEN);
+    printf("Sending directory: %s\n", argv[3]);
+
+    struct dirent *entry;
+    pthread_t threads[256]; // 최대 256개의 파일 업로드 스레드
+    int thread_count = 0;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_REG) { // 일반 파일인 경우
+            char file_path[FILE_LEN];
+            snprintf(file_path, sizeof(file_path), "%s/%s", argv[3], entry->d_name);
+
+            upload_arg *arg = (upload_arg *)malloc(sizeof(upload_arg));
+            arg->sock = sd;
+            strcpy(arg->file_path, file_path);
+
+            pthread_create(&threads[thread_count++], NULL, upload_file, (void *)arg);
+            usleep(100000); // 스레드 생성 간격 조정
+        }
+    }
+
+    closedir(dir);
+
+    // 모든 스레드가 끝날 때까지 대기
+    for (int i = 0; i < thread_count; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    shutdown(sd, SHUT_WR);
+    close(sd);
+    return 0;
+}
+
+void error_handling(char *message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
