bea9e79107cf4fa87a5df3dab2919092dc0cf2a1
root <22000082@handong.ac.kr>
2024-12-04 13:11:00 +0000

    done

--- hw_clnt.c
+++ hw_clnt.c
@@ -22,7 +22,9 @@ typedef struct File_data
 
 pthread_t thread; 
 int count=0;
+int uploaded=0;
 struct sockaddr_in serv_adr;
+pthread_mutex_t mutx;
 
 
 void error_handling(char *message);
@@ -86,7 +88,6 @@ void list_files_for_thread(char *dir_path) {
         }
 
         if (S_ISREG(check_dir.st_mode)) {
-            printf("File: %s\n", full_path);
 			strcpy(file_data_to_send.file_name,full_path);
 			pthread_create(&thread, NULL, handle_client, (void*)&file_data_to_send);
 			pthread_join(thread,NULL);
@@ -122,8 +123,6 @@ int main(int argc, char *argv[])
 	
 
 	list_files_for_count(argv[3]);
-	printf("%d === \n",count);
-	//file_data = (File_data *)malloc(sizeof(File_data)*count);
 	list_files_for_thread(argv[3]);
 	
 
@@ -131,6 +130,8 @@ int main(int argc, char *argv[])
 	write(sd, &count, sizeof(count));
 
 	close(sd);
+	printf("%d/%d files has been uploaded!\n",uploaded,count);
+
 	return 0;
 }
 
@@ -155,14 +156,12 @@ void *handle_client(void *arg)
 
  
 
-    printf("%d =======thread hit for %s\n", clnt_sock,file_data.file_name);
     //파일 이름 보내기
 	write(clnt_sock, &file_data, sizeof(file_data));
 
 	//파일 보내기
 	fp = fopen(file_data.file_name, "rb");
 	
-	printf("hit fp\n");
 	while(1)
 		{
 			read_cnt = fread((void*)msg, 1, BUF_SIZE, fp);
@@ -171,7 +170,6 @@ void *handle_client(void *arg)
 			if (read_cnt < BUF_SIZE)
 			{
 				write(clnt_sock, msg, read_cnt);
-				printf("%d :: Sent %d bytes in %s\n", clnt_sock,read_size,file_data.file_name);
 				break;
 			}
 			write(clnt_sock, msg, BUF_SIZE);
@@ -180,6 +178,11 @@ void *handle_client(void *arg)
 	fclose(fp);
 	close(clnt_sock);
 	
+	printf("[%s] Upload completed!...%dbytes\n",file_data.file_name,read_size);
+
+	pthread_mutex_lock(&mutx);
+	uploaded++;
+	pthread_mutex_unlock(&mutx);
 	return NULL;
 }
 
